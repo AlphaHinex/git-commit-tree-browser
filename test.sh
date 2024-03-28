0a570bc8ba728e51f11fde77ac4472234a3ffcfa
Alpha Hinex <AlphaHinex@gmail.com>
2024-03-28 17:27:00 +0800

    Generate txt file to current path

--- test.sh
+++ test.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 repo=AIAS
+
 rm -f *.txt
 
 # 使用ls命令按时间排序并获取文件列表，然后使用awk提取文件名
 for commit in $(git --git-dir=$repo/.git log --all --pretty=format:'%h'); do
   git --git-dir=$repo/.git --work-tree=$repo checkout $commit
-  tree -N -d -L 2 > ../"$commit".txt
-  echo $commit >> ../"$commit".txt
+  tree -N -d -L 2 > "$commit".txt
+  echo $commit >> "$commit".txt
 done
 
